0d2a1e59317b098643809fe5dcb6f6123272ed30
bk62 <bkafle862@gmail.com>
2019-09-07 21:51:19 +0545

    Genrated a simple rect mesh - upto section 4 in tutorial

---
+++ Assets/Grid.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (MeshFilter), typeof (MeshRenderer))]
+public class Grid : MonoBehaviour {
+    // num tiles in x and y
+    public int xSize, ySize;
+
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    private void Awake () {
+        // StartCoroutine (Generate ());
+        Generate();
+    }
+
+    private void Generate () {
+        // private IEnumerator Generate () {
+        // WaitForSeconds wait = new WaitForSeconds (0.05f);
+
+        GetComponent<MeshFilter> ().mesh = mesh = new Mesh ();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        // place vertices in grid 
+        for (int i = 0, y = 0; y <= ySize; y++) {
+            for (int x = 0; x <= xSize; x++, i++) {
+                vertices[i] = new Vector3 (x, y);
+            }
+        }
+
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+                // mesh.triangles = triangles;
+                // yield return wait;
+            }
+        }
+
+        mesh.triangles = triangles;
+    }
+
+    private void OnDrawGizmos () {
+        // draw spheres at each vertex
+        if (vertices == null) {
+            // dont draw if no vertices
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere (vertices[i], 0.1f);
+        }
+    }
+
+}
